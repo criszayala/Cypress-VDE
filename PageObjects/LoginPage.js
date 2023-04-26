996d6d7b06b46ff252b01201d6a152b637ec8486
cristian ayala <cristian.adrian1993@gmail.com>
2023-04-26 20:30:35 -0300

    Empty lines are removed from 'LoginPage.js'

--- PageObjects/LoginPage.js
+++ PageObjects/LoginPage.js
@@ -4,22 +4,18 @@ setUserName(username)
     {
         cy.get("input[placeholder='Username']").type(username);
     }
-
 setPassword(password)
     {
         cy.get("input[placeholder='Password']").type(password);
     }
-
 clickSubmit()
     {
         cy.get("button[type='submit']").click();
     }
-
 verifyLogin()
     {
         cy.get(".oxd-topbar-header-breadcrumb > .oxd-text").should('have.text', 'Dashboard');
     }
-
 }
 //Se exporta la clase para poder utilizarla
 export default Login;
